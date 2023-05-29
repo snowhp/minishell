a42b47e4b96c2511b0650be8a51431c2fc1d03fe
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-05-29 14:50:59 +0100

    Fix: Missing function declaration

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 14:28:23 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/05/29 14:31:58 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/05/29 14:48:14 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include <stdio.h>
 # include "get_next_line/get_next_line.h"
 # include "libft/libft.h"
+
+void	ft_loop(void);
 
 #endif
