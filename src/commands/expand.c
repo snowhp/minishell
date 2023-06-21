2dc13d617e72da2f53ec71192c393957545d899f
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-06-21 23:05:21 +0100

    Fix: compile errors

--- src/commands/expand.c
+++ src/commands/expand.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 14:33:42 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/06/21 14:47:03 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/06/21 23:04:00 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ void	ft_expand(t_mini *complex, t_data **info)
 		while (complex->simplecommands[cmds].arguments[i])
 		{
 			if (complex->simplecommands[cmds].arguments[i][0] == '\'')
-				complex->simplecommands[cmds].arguments[i] = ft_removequotes(&complex->simplecommands[cmds].arguments[i]);
+				complex->simplecommands[cmds].arguments[i] = ft_removequotes(complex->simplecommands[cmds].arguments[i]);
 			else if (complex->simplecommands[cmds].arguments[i][0] == '"')
 			{
-				complex->simplecommands[cmds].arguments[i] = ft_removequotes(&complex->simplecommands[cmds].arguments[i]);
-				complex->simplecommands[cmds].arguments[i] = ft_replacevar(&complex->simplecommands[cmds].arguments[i], info);
+				complex->simplecommands[cmds].arguments[i] = ft_removequotes(complex->simplecommands[cmds].arguments[i]);
+				complex->simplecommands[cmds].arguments[i] = ft_replacevar(complex->simplecommands[cmds].arguments[i], info);
 			}
 			else
-				complex->simplecommands[cmds].arguments[i] = ft_replacevar(&complex->simplecommands[cmds].arguments[i], info);
+				complex->simplecommands[cmds].arguments[i] = ft_replacevar(complex->simplecommands[cmds].arguments[i], info);
 			i++;
 		}
 		cmds++;
@@ -72,20 +72,27 @@ char *ft_removequotes(char *str)
 	return (result);
 }
 
-/* char *ft_replacevar(char *str, t_data **info)
+char *ft_replacevar(char *str, t_data **info)
 {
-	char	*result;
-	char	*var;
+	//char	*result;
+	//char	*var;
 	int	i;
+	int	size;
+	(void) info;
 
+	size = 0;
 	i = 0;
 	while(str[i])
 	{
 		if (str[i] == '$' && str[i + 1])
+		{
+			if (str)
 			break;
+		}
+		size++;
 		i++;
 	}
-	if (i = ft_strlen(str))//If no $ followed by anything is found
+	if ((size_t)i == ft_strlen(str))//If no $ followed by anything is found
 		return (str);
 	i = 0;
 	while (str[i])
@@ -96,4 +103,6 @@ char *ft_removequotes(char *str)
 		}
 		i++;
 	}
-} */
+	return 0;
+}
+
