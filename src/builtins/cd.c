e0882775aa4c3281c860a3b53678d89e3430af12
tiagohtavares <117377680+tiagohtavares@users.noreply.github.com>
2023-06-29 16:29:12 +0100

    Fixing cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: ttavares <ttavares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 16:43:35 by ttavares          #+#    #+#             */
-/*   Updated: 2023/06/28 16:59:10 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/06/29 16:28:20 by ttavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,19 @@ void	ft_changedir(char *path, t_data **info)
 	char	currentdir[200];
 	char	*old_dir;
 
+	old_dir = ft_find_env(info, "OLDPWD");
+	getcwd(currentdir, sizeof(currentdir));
 	if (path)
 	{
-		old_dir = ft_find_env(info, "OLDPWD");
-		getcwd(currentdir, sizeof(currentdir));
 		if (ft_strlen(path) == 1 && path[0] == '-')
-		{
 			chdir(old_dir);
-			old_dir = currentdir;
-		}
 		else
-		{
-			old_dir = ft_strdup(currentdir);
-			ft_strlcat(currentdir, "/", ft_strlen(currentdir) + 2);
-			ft_strlcat(currentdir, path, (ft_strlen(currentdir) + ft_strlen(path)) + 1);
-			chdir(currentdir);
-		}
-		printf("%s\n", currentdir);
-		ft_update_env(info, "OLDPWD", old_dir);
+			chdir(path);
 	}
+	else if (!path)
+		chdir("/home");
+	old_dir = currentdir;
+	ft_update_env(info, "OLDPWD", currentdir);
+	getcwd(currentdir, sizeof(currentdir));
+	printf("%s\n", currentdir);
 }
