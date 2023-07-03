b662e21e27ea7c166f49dc004a4fdf359b1b41e3
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-07-03 14:17:33 +0100

    Undo on commit

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 18:11:54 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/07/03 14:09:01 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/07/03 14:13:13 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	handle_ctrlc(int sig)
 	(void) sig;
 	write(1, "\n", 1);
 	rl_replace_line("", 0);
+	rl_on_new_line();
 	rl_redisplay();
 	g_estatus = 130;
 }
