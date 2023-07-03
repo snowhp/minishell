46b433215e71d03740b815b647c4703c27f62e75
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-07-03 15:05:07 +0100

    Fix: Better exit with exit codes

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:34:14 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/07/02 19:48:23 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/07/03 15:04:20 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 void	ft_exit(char **args, t_mini *c, t_data **info)
 {
+	if (args[1] && ((ft_atoi(args[1])) || (args[1][0] == '0' && !args[1][1])))
+		g_estatus = ft_atoi(args[1]);
+	if (args[2])
+	{
+		ft_putstr_fd("exit: too many arguments\n", 2);
+		g_estatus = 1;
+		return ;
+	}
 	if (args[1] && !ft_isnum(args[1]))
 	{
 		ft_putstr_fd("exit: ", 2);
 		ft_putstr_fd(args[1], 2);
 		ft_putstr_fd(": numeric argument required\n", 2);
+		g_estatus = 2;
 	}
-	else if (args[2])
-		ft_putstr_fd("exit: too many arguments\n", 2);
-	else
-	{
-		if (args[1] && ((ft_atoi(args[1])) || (args[1][0] == '0' && !args[1][1])))
-			g_estatus = ft_atoi(args[1]);
-		ft_freearray(c->args);
-		free(c->line);
-		ft_freearray((*info)->env);
-		exit(g_estatus);
-	}
+	ft_freearray(c->args);
+	free(c->line);
+	ft_freearray((*info)->env);
+	exit(g_estatus);
 }
