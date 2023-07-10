eb1ec6bd077c82ceae6639c9e7ce4a32c65acb3e
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-07-10 11:02:52 +0100

    Fix: Exitstatus on command not found

--- src/commands/commands2.c
+++ src/commands/commands2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   commands2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttavares <ttavares@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 12:34:55 by ttavares          #+#    #+#             */
-/*   Updated: 2023/07/07 12:36:16 by ttavares         ###   ########.fr       */
+/*   Updated: 2023/07/10 11:01:50 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,18 @@ void	ft_execute(char **cmd, t_data **info)
 		path = ft_path(cmd[0], (*info)->env);
 	if (!path)
 	{
-		ft_putstr_fd(cmd[0], 2);
-		ft_putstr_fd(" : command not found\n", 2);
-		exit(errno);
+		if (ft_countchar(cmd[0], '/') >= 1)
+		{
+			ft_putstr_fd(cmd[0], 2);
+			ft_putstr_fd(" : No such file or directory\n", 2);
+		}
+		else
+		{
+			ft_putstr_fd(cmd[0], 2);
+			ft_putstr_fd(" : command not found\n", 2);
+		}
+		g_estatus = 127;
+		exit(g_estatus);
 	}
 	if (execve(path, cmd, (*info)->env) == -1)
 		exit(errno);
